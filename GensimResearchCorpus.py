f23079e27f1e6ada3a741eceb17e53cf0b9b18ab
Rohit Apte <rohit@octobabel.io>
2021-06-27 11:03:11 +0800

    doc2vec basic model

--- GensimResearchCorpus.py
+++ GensimResearchCorpus.py
@@ -5,8 +5,9 @@ from data_utilities import clean_text, DATA_DIR
 
 
 class GensimResearchCorpus(object):
-    def __init__(self, tokens_only=False):
+    def __init__(self, tokens_only=False, filterCategory=None):
         self.tokens_only = tokens_only
+        self.filterCategory = filterCategory
 
     def __iter__(self):
         """
@@ -19,6 +20,9 @@ class GensimResearchCorpus(object):
         with open(DATA_DIR + 'arxiv-metadata-oai-snapshot.json') as f:
             for i, line in enumerate(f):
                 data = json.loads(line)
+                if self.filterCategory is not None:
+                    if self.filterCategory not in data['categories']:
+                        continue
                 data['abstract'] = data['abstract'].strip().replace('\n', ' ')
                 data['abstract_cleaned'] = clean_text(data['abstract'])
                 tokens = word_tokenize(data['abstract_cleaned'])
@@ -27,6 +31,36 @@ class GensimResearchCorpus(object):
                 else:
                     yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
 
+class ResearchCorpus(object):
+    def __init__(self, tokens_only=False, filterCategory=None):
+        self.filterCategory = filterCategory
+
+    def __iter__(self):
+        """
+        loads the arxiv metadata from file. there are duplicate entries if there has been an update.
+        seems like the later entries are more current
+        :return:
+            unique_id_count - count of duplicates
+            return_data - dictionary hashed by id of metadata
+        """
+        with open(DATA_DIR + 'arxiv-metadata-oai-snapshot.json') as f:
+            for i, line in enumerate(f):
+                data = json.loads(line)
+                if self.filterCategory is not None:
+                    if self.filterCategory not in data['categories']:
+                        continue
+                data['title'] = data['title'].strip().replace('\n', ' ')
+                data['abstract'] = data['abstract'].strip().replace('\n', ' ')
+                data['abstract_cleaned'] = clean_text(data['abstract'])
+                data['tokens'] = word_tokenize(data['abstract_cleaned'])
+                yield data
+
 if __name__=='__main__':
-    for item in GensimResearchCorpus():
-        print(item)
+    model = gensim.models.doc2vec.Doc2Vec(workers=8,
+                                          vector_size=50,
+                                          min_count=5,
+                                          epochs=20)
+    mycorpus = GensimResearchCorpus(filterCategory='cs.CG')
+    model.build_vocab(mycorpus)
+    model.train(mycorpus, total_examples=model.corpus_count, epochs=model.epochs)
+    model.save('doc2vec.model')
